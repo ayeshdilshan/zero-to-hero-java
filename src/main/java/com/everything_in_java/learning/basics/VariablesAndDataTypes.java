98388f1423e5c6df8d220ce97d79195f2b989fb8
ayesh <ayeshjayasundara0725@gmail.com>
2024-10-19 00:08:39 +0530

    variables and data types

---
+++ src/main/java/com/everything_in_java/learning/basics/VariablesAndDataTypes.java
@@ -0,0 +1,22 @@
+package com.everything_in_java.learning.basics;
+
+public class VariablesAndDataTypes {
+    public static void main(String args[]){
+        int num1 = 5;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println("num1 is " + num1);
+        System.out.println("num2 is " + num2);
+        System.out.println("Total is " + result);
+
+        double salary = 550.50;
+        System.out.println("Salary is " + salary);
+
+        char grade = 'A';
+        System.out.println("grade is " + grade);
+
+        boolean isStudent = false;
+        System.out.println("Is a student? " + isStudent);
+
+    }
+}
