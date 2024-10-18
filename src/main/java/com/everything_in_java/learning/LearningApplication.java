2ebd6088d4df09754cc03a283a4b0ec622a52057
ayesh <ayeshjayasundara0725@gmail.com>
2024-10-18 13:55:34 +0530

    initial commit

---
+++ src/main/java/com/everything_in_java/learning/LearningApplication.java
@@ -0,0 +1,13 @@
+package com.everything_in_java.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everything_in_java/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everything_in_java.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
