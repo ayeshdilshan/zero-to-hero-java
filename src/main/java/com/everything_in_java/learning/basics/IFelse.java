623528780d96f7af1ca588868da9894c255fc3c8
ayesh <ayeshjayasundara0725@gmail.com>
2024-10-19 13:18:10 +0530

    if else statement in java
    basic if-else/ if-else if-else/ nested if-else

---
+++ src/main/java/com/everything_in_java/learning/basics/IFelse.java
@@ -0,0 +1,43 @@
+package com.everything_in_java.learning.basics;
+
+public class IFelse {
+
+    public static void main(String args[]){
+
+        //basic if else
+
+        int number = 10;
+        int total = 0;
+
+        if(number>0){
+            System.out.println("number is positive");
+        }else {
+            System.out.println("number is positive");
+        }
+
+        //if-else if-else
+
+        if(total>0){
+            System.out.println("total is greater than zeo");
+        } else if (total<0) {
+            System.out.println("total is lesee than zeo");
+        } else {
+            System.out.println("total is equal to zeo");
+        }
+
+        //Nested if-else
+
+        int age = 10;
+        boolean hasLicense = true;
+
+        if(age>=18){
+            if (hasLicense){
+                System.out.println("you are eligible to drive");
+            }else {
+                System.out.println("you are not eligible to drive without a license");
+            }
+        }else {
+                System.out.println("you are too young to drive");
+        }
+    }
+}
