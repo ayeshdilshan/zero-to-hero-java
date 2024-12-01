d9d9fe9db07bb445f11336628675bac0b08f3b03
ayesh <ayeshjayasundara0725@gmail.com>
2024-12-02 00:36:52 +0530

    entity relationship
    many to one , many to many

---
+++ src/main/java/com/everything_in_java/learning/springDataJpaMapping/controller/CourseController.java
@@ -0,0 +1,9 @@
+package com.everything_in_java.learning.springDataJpaMapping.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+}
